b9c4bf016885d74b7821c7cd53143e8503265dd7
yguaj <yguaj@DESKTOP-0GNQQKF>
2023-11-30 13:11:52 -0600

    Agregar archivos de proyecto.

---
+++ SistemaVenta.AplicacionWeb/wwwroot/js/vistas/DashBoard_Index.js
@@ -0,0 +1,132 @@
+﻿
+
+$(document).ready(function () {
+
+
+    $("div.container-fluid").LoadingOverlay("show");
+
+    fetch("/DashBoard/ObtenerResumen")
+        .then(response => {
+            $("div.container-fluid").LoadingOverlay("hide");
+            return response.ok ? response.json() : Promise.reject(response);
+        })
+        .then(responseJson => {
+
+            if (responseJson.estado) {
+
+
+                //Mostrar datos para las tarjetas
+                let d = responseJson.objeto
+
+                $("#totalVenta").text(d.totalVentas)
+                $("#totalIngresos").text(d.totalIngresos)
+                $("#totalProductos").text(d.totalProductos)
+                $("#totalCategorias").text(d.totalCategorias)
+
+                //Obtener textos y valores para nuestro grafico de barras
+                let barchart_labels;
+                let barchart_data;
+
+                if (d.ventasUltimaSemana.length > 0) {
+                    barchart_labels = d.ventasUltimaSemana.map((item) => { return item.fecha })
+                    barchart_data = d.ventasUltimaSemana.map((item) => { return item.total })
+                } else {
+                    barchart_labels = ["sin resultados"]
+                    barchart_data = [0]
+                }
+
+
+                //obtener textos y valores para nuestro grafico de pie
+                let piechar_labels;
+                let piechart_data;
+                if (d.productosTopUltimaSemana.length > 0) {
+                    piechar_labels = d.productosTopUltimaSemana.map((item) => { return item.producto })
+                    piechart_data = d.productosTopUltimaSemana.map((item) => { return item.cantidad })
+                } else {
+                    piechar_labels = ["sin resultados"]
+                    piechart_data = [0]
+                }
+
+
+                // Bar Chart Example
+                let controlVenta = document.getElementById("charVentas");
+                let myBarChart = new Chart(controlVenta, {
+                    type: 'bar',
+                    data: {
+                        labels: barchart_labels,
+                        datasets: [{
+                            label: "Cantidad",
+                            backgroundColor: "#75C9C1",
+                            hoverBackgroundColor: "#75C9C1",
+                            borderColor: "#75C9C1",
+                            data: barchart_data,
+                        }],
+                    },
+                    options: {
+                        maintainAspectRatio: false,
+                        legend: {
+                            display: false
+                        },
+                        scales: {
+                            xAxes: [{
+                                gridLines: {
+                                    display: false,
+                                    drawBorder: false
+                                },
+                                maxBarThickness: 50,
+                            }],
+                            yAxes: [{
+                                ticks: {
+                                    min: 0,
+                                    maxTicksLimit: 5
+                                }
+                            }],
+                        },
+                    }
+                });
+
+                // Pie Chart Example
+                let controlProducto = document.getElementById("charProductos");
+                let myPieChart = new Chart(controlProducto, {
+                    type: 'doughnut',
+                    data: {
+                        labels: piechar_labels,
+                        datasets: [{
+                            data: piechart_data,
+                            backgroundColor: ['#D676A4', '#75C9C1', '#9E75C9', "#E7F382"],
+                            hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf', "#FF5733"],
+                            hoverBorderColor: "rgba(234, 236, 244, 1)",
+                        }],
+                    },
+                    options: {
+                        maintainAspectRatio: false,
+                        tooltips: {
+                            backgroundColor: "rgb(255,255,255)",
+                            bodyFontColor: "#858796",
+                            borderColor: '#dddfeb',
+                            borderWidth: 1,
+                            xPadding: 15,
+                            yPadding: 15,
+                            displayColors: false,
+                            caretPadding: 10,
+                        },
+                        legend: {
+                            display: true
+                        },
+                        cutoutPercentage: 80,
+                    },
+                });
+
+
+
+
+
+
+            }
+
+
+
+        })
+
+
+})
